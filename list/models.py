122df2f328f0fd375f28587112daf14190d50896
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-01-14 23:18:39 +0200

    Adding models to the app

--- list/models.py
+++ list/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Relationship(models.Model):
+    name = models.CharField(max_length=50, null=True)
+
+
+class Contact(models.Model):
+    full_name = models.CharField(max_length=150, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(max_length=150, blank=True, null=True)
+    relationship = models.ForeignKey(Relationship, on_delete=models.CASCADE)
+    company = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=250, null=True, blank=True)
+
+
+
+
+
+
